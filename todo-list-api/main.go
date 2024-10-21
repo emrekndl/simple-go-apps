a2054b21901873957fb5026eb03013a144792265
emrekndl <kondulemre@gmail.com>
2024-10-22 02:13:27 +0300

    todo list api updated.

--- todo-list-api/main.go
+++ todo-list-api/main.go
@@ -11,30 +11,25 @@ import (
 )
 
 func main() {
-	// Veritabanını başlat
 	db := repository.InitDB("todos.db")
 	defer db.Close()
 
-	// Repository ve Service katmanlarını oluştur
 	todoRepo := repository.NewTodoRepository(db)
 	todoService := service.NewTodoService(todoRepo)
 
-	// Router'ı oluştur
 	r := mux.NewRouter()
 
 	r.Use(todoService.ContentTypeMiddleware)
 
-	// Routes
 	r.HandleFunc("/todos", todoService.GetTodos).Methods("GET")
 	r.HandleFunc("/todos/{id}", todoService.GetTodo).Methods("GET")
 	r.HandleFunc("/todos", todoService.CreateTodo).Methods("POST")
 	r.HandleFunc("/todos/{id}", todoService.UpdateTodo).Methods("PUT")
 	r.HandleFunc("/todos/{id}", todoService.DeleteTodo).Methods("DELETE")
 
-	// Sunucuyu http.Server ile başlat
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8080", // IP ve port
+		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 		IdleTimeout:  60 * time.Second,
